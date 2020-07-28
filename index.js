2bcfc118f388bc3336d9a3606e6a52c518865f2b
Sebastian Gilarranz <sebas@gilarranz.me>
2020-07-28 03:09:59 -0400

    cors

--- index.js
+++ index.js
@@ -9,11 +9,8 @@ var cors = require('cors');
 
 require("dotenv").config(); // FOR LOCAL USE ONLY
 
-app.listen(process.env.PORT || 5000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+const port = process.env.PORT || 5000;
 const app = express();
-app.use(cors());
 
 require("./startup/passport/passport-setup")();
 require("./startup/db")();
@@ -33,6 +30,7 @@ app.use(
   })
 );
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
